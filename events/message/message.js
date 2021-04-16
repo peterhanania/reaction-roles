5219667a7f88eb9b5f93e095daa933039f6748ee
peterhanania <72696414+peterhanania@users.noreply.github.com>
2021-04-16 19:12:13 +0300

    fixed cooldown bug

--- events/message/message.js
+++ events/message/message.js
@@ -1,12 +1,13 @@
 const Event = require('../../structures/Event');
 const { Permissions, Collection } = require("discord.js");
-const moment = require('moment');
 const discord = require("discord.js");
 const config = require('./../../config.json');
 const { MessageEmbed } = require('discord.js');
 const mongoose = require('mongoose');
-const Guild = require('../../database/guild')
+const Guild = require('../../database/guild');
 const permissions = require('../../permissions.json');
+const moment = require('moment');
+require("moment-duration-format");
 
 module.exports = class extends Event {
   constructor(...args) {
@@ -29,7 +30,8 @@ module.exports = class extends Event {
 
   async run(message) {
     try {
-    
+     
+
       const mentionRegex = RegExp(`^<@!?${this.client.user.id}>$`);
       const mentionRegexPrefix = RegExp(`^<@!?${this.client.user.id}>`);
     
@@ -45,14 +47,10 @@ module.exports = class extends Event {
             guildId: message.guild.id,
             prefix: config.prefix || 'rr!',
           });
-
-          return message.channel.send(`Successfuly added ${message.guild.name} to the database. Bot works now.`)
         }
       });
-
-    
-    
-      if (message.content.match(mentionRegex)) {
+      
+     if (message.content.match(mentionRegex)) {
 
        message.channel.send(`Hello there! My prefix for this server is ${settings.prefix || 'rr!' }`).then((s)=>{
          s.delete({ timeout: 10000 })
@@ -60,36 +58,50 @@ module.exports = class extends Event {
 
       }
 
-
-
-
-      let mainPrefix = settings ? settings.prefix : 'rr!';
+      let mainPrefix = settings ? settings.prefix : '!';
 
       const prefix = message.content.match(mentionRegexPrefix) ? 
         message.content.match(mentionRegexPrefix)[0] : mainPrefix 
 
 
+      const guildDB = await Guild.findOne({
+        guildId: message.guild.id
+      });
+
+      
+      moment.suppressDeprecationWarnings = true;
+
 
         
+
       if (!message.content.startsWith(prefix)) return;
       
-    
+      // eslint-disable-next-line no-unused-vars  
       const [cmd, ...args] = message.content.slice(prefix.length).trim().split(/ +/g);
       const command = this.client.commands.get(cmd.toLowerCase()) || this.client.commands.get(this.client.aliases.get(cmd.toLowerCase()));
 
 
 
 
+  
 
-   
+      
+
+
       if (command) {
        
+
+     
+   
+
+
         const rateLimit = this.ratelimit(message, cmd);
 
         if (!message.channel.permissionsFor(message.guild.me).has('SEND_MESSAGES')) return;
 
-        //check for cooldown
-        if (typeof rateLimit === "string") return message.channel.send(`Please wait **${rateLimit}** before running the **${cmd}** command again - ${message.author}`);
+    
+  
+        if (typeof rateLimit === "string") return message.channel.send(` ${message.client.emoji.fail} Please wait **${rateLimit}** before running the **${cmd}** command again - ${message.author}`);
   
 
         if (command.botPermission) {
@@ -100,10 +112,10 @@ module.exports = class extends Event {
           if (missingPermissions.length !== 0) {
        const embed = new MessageEmbed()
         .setAuthor(`${this.client.user.tag}`, message.client.user.displayAvatarURL({ dynamic: true }))
-        .setTitle(` Missing Bot Permissions`)
+        .setTitle(`<:wrong:822376943763980348> Missing Bot Permissions`)
         .setDescription(`Command Name: **${command.name}**\nRequired Permission: **${missingPermissions.map(p => `${p}`).join(' - ')}**`)
         .setTimestamp()
-        .setFooter('https://github.com/peterhanania/reaction-roles/')
+        .setFooter('https://pogy.xyz')
         .setColor(message.guild.me.displayHexColor);
       return message.channel.send(embed).catch(()=>{})
           }
@@ -119,10 +131,10 @@ module.exports = class extends Event {
       if (missingPermissions.length !== 0) {
         const embed = new MessageEmbed()
           .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
-          .setTitle(`Missing User Permissions`)
+          .setTitle(`<:wrong:822376943763980348> Missing User Permissions`)
           .setDescription(`Command Name: **${command.name}**\nRequired Permission: **${missingPermissions.map(p => `${p}`).join('\n')}**`)
           .setTimestamp()
-          .setFooter('https://github.com/peterhanania/reaction-roles/')
+          .setFooter('https://pogy.xyz')
           .setColor(message.guild.me.displayHexColor);
        return message.channel.send(embed).catch(()=>{})
       }
@@ -135,27 +147,31 @@ module.exports = class extends Event {
           if (!this.client.config.developers.includes(message.author.id)) return
         }
 
-        if (command.disabled) return message.channel.send(`The owner has disabled the following command for now. Try again Later!`)
+        if (command.disabled) return message.channel.send(`The owner has disabled the following command for now.`)
+    
+
+  
         
         await this.runCommand(message, cmd, args)
 
         .catch(error => {
-        console.log(error);
-        return message.channel.send(`An Error has occured, Please let the developer know.`)
+           console.log(error)
+     return message.channel.send(`An Error has occured, please let the developer know.`)
         })
       }
     } catch(error) {
-      console.log(error);
-      return message.channel.send(`An Error has occured, Please let the developer know.`)
+      console.log(error)
+     return message.channel.send(`An Error has occured, please let the developer know.`)
     }
   } 
 
     async runCommand(message, cmd, args) {
 
         if (!message.channel.permissionsFor(message.guild.me) || !message.channel.permissionsFor(message.guild.me).has('EMBED_LINKS'))
-          return message.channel.send(`Missing bot Permissions - **Embeds Links**`)
+          return message.channel.send(`${message.client.emoji.fail} Missing bot Permissions - **Embeds Links**`)
 
         const command = this.client.commands.get(cmd.toLowerCase()) || this.client.commands.get(this.client.aliases.get(cmd.toLowerCase()));
+
       
         await command.run(message, args)
     }
@@ -166,19 +182,19 @@ module.exports = class extends Event {
         if (message.author.permLevel > 4) return false;
     
         const cooldown = command.cooldown * 1000
-        const ratelimits = this.ratelimits.get(message.author.id) || {}; 
-        if (!ratelimits[command.name]) ratelimits[command.name] = Date.now() - cooldown; 
-        const difference = Date.now() - ratelimits[command.name];
-        if (difference < cooldown) { 
-          return moment.duration(cooldown - difference).format("D [days], H [hours], m [minutes], s [seconds]", 1); 
+        const ratelimits = this.ratelimits.get(message.author.id) || {}; // get the ENMAP first.
+        if (!ratelimits[command.name]) ratelimits[command.name] = Date.now() - cooldown; // see if the command has been run before if not, add the ratelimit
+        const difference = Date.now() - ratelimits[command.name]; // easier to see the difference
+        if (difference < cooldown) { // check the if the duration the command was run, is more than the cooldown
+          return moment.duration(cooldown - difference).format("D [days], H [hours], m [minutes], s [seconds]", 1); // returns a string to send to a channel
         } else {
-          ratelimits[command.name] = Date.now(); 
-          this.ratelimits.set(message.author.id, ratelimits); 
+          ratelimits[command.name] = Date.now(); // set the key to now, to mark the start of the cooldown
+          this.ratelimits.set(message.author.id, ratelimits); // set it
           return true;
         }
-      } catch(error) {
-        console.log(error);
-        return message.channel.send(`An Error has occured, Please let the developer know.`)
+      } catch(e) {
+        console.log(e)
+        message.channel.send(`An Error has occured, please let the developer know.`)
       }
     }
 }
